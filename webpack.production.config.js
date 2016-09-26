0fea8445148572e4822d44e794b9a2e04249d554
tianzx <tianzx@aliyun.com>
2016-09-26 18:30:24 +0800

    dev

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -24,7 +24,7 @@ module.exports = {
             }
         }),
         new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.OccurenceOrderPlugin(),
+        // new webpack.optimize.OccurenceOrderPlugin(),
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false,
@@ -34,7 +34,7 @@ module.exports = {
             },
         }),
         new webpack.NoErrorsPlugin(),
-        new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor-[chunkhash:6].js'),
+        new webpack.optimize.CommonsChunkPlugin({name:'vendor', filename:'vendor-[chunkhash:6].js'}),
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify('production')
         }),
